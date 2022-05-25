fae700c5f297939d49617f7c758f11466be8bc4e
danishdyabala2519-85 <danishdeepasoman02@gmail.com>
2022-05-25 11:16:59 +0530

    this is error handling menu driven program

---
+++ error.py
@@ -0,0 +1,43 @@
+def div(a,b):
+    c=a/0
+    print("zero  division error ")
+  
+def add(a,b):
+    c=a+b
+    print(d)
+    print("name error example")
+  
+def sub(a,b):
+    printf("sub is :",a-b)
+    print("IO error example")
+  
+def lis():
+    lis=[1,2,3,4,5]
+    lis(9)
+    print(lis)
+ 
+  
+while True:  
+    print("\nMAIN MENU")  
+    print("1. zero division error ")
+    print("2. name error ")
+    print("3. IO error example")
+    print("4.index error example")
+    print("5. exit")
+    choice2= int(input("Enter the Choice:"))  
+  
+    
+    if choice2 == 1:  
+        div(8,9)
+              
+    elif choice2 == 2:  
+        add(5,5)
+              
+    elif choice2 == 3:  
+        sub(8,6)
+  
+    elif choice2 == 4:  
+        lis
+        
+    else:  
+        print("Oops! Incorrect Choice.")  
